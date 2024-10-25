39388572545b584aba1c8e5d396a542b065c0687
kayneluo <chienweiluo@gmail.com>
2024-10-26 01:09:42 +0800

    [fix] basic setting for static

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <script src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
-import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [displayName, setDisplayName] = useState("");
+  
+  const closeLiff = () => {
+    const liff = window.liff;
+    liff.closeWindow();
+  }
+
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', function () {
+      // 初始化 LIFF 應用
+      liff.init({ liffId: '2006500949-YMqZ2gbp' })
+        .then(() => {
+          if (!liff.isLoggedIn()) {
+            liff.login(); // 如果用戶未登入，則進行登入
+          } else {
+            getUserProfile(); // 如果已登入，獲取用戶資料
+          }
+        })
+        .catch(err => {
+          console.error('LIFF 初始化失敗:', err);
+        });
+
+      // 獲取用戶資料
+      function getUserProfile() {
+        liff.getProfile()
+          .then(profile => {
+            setDisplayName(profile.displayName);
+          })
+          .catch(err => {
+            console.error('無法獲取用戶資料:', err);
+          });
+      }
+    });
+  }, []);
   return (
     <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
+      <h1>{displayName}</h1>
+      <button onClick={closeLiff}>關閉</button>
     </div>
   );
 }
